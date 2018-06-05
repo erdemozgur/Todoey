61f2d431ecd48a7034c6c573dfe5cd099daecca3
erdem ozgur <erdemozgur@erdems-macbook-pro.local>
2018-06-05 23:08:43 +0300

    Add new item functionality complete.

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-    let  itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var  itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +30,7 @@ class ToDoListViewController: UITableViewController {
     
     // MARK : TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(itemArray[indexPath.row])
+        // print(itemArray[indexPath.row])
         
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
@@ -42,6 +42,27 @@ class ToDoListViewController: UITableViewController {
         // Elemanlari ekranda sectigimizde arka plan gri degil beyaz olur.
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    // MARK : Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item ", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+        
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+            // what will happen when the user press alert  "add new item"
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
